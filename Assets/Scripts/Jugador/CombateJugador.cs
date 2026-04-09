0a26c24276db45b8ad8ac5b9c30b8804c0d818cc
andvigofp <aniamcion96@gmail.com>
2026-04-09 16:11:57 +0200

    Añadido la animacion del Juagador de ataque, creado un scrip para combatecuerpo a cuerpo, configurando, para que pueda atacar en un determinado tiempo, otro scrip vida del enemigo para configurar la vida que tiene, hasta que llegue a zero y se destruya al perder vida

---
+++ Assets/Scripts/Jugador/CombateJugador.cs
@@ -0,0 +1,57 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CombateJugador : MonoBehaviour
+{   
+    private const string STRING_ANIMACION_ATAQUE = "Atacar";
+
+    [Header("Refrerencias")]
+    [SerializeField] private Animator animator;
+
+    [Header("Ataque")]
+    [SerializeField] private Transform controladorAtaque;
+    [SerializeField] private float radioAtaque;
+    [SerializeField] private int dañoAtaque;
+    [SerializeField] private float tiempoEntreAtaques;
+    [SerializeField] private float tiempoUltimoAtaque;
+
+    private void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            IntentarAtacar();
+        }
+    }
+
+    private void IntentarAtacar()
+    {
+        if(Time.time < tiempoUltimoAtaque + tiempoEntreAtaques) { return; }
+        {
+            Atacar();
+        }
+    }
+
+    private void Atacar()
+    {
+        animator.SetTrigger(STRING_ANIMACION_ATAQUE);
+
+        tiempoUltimoAtaque = Time.time;
+        
+        Collider2D[] objetosTocados = Physics2D.OverlapCircleAll(controladorAtaque.position, radioAtaque);
+
+
+        foreach (Collider2D objecto in objetosTocados)
+        {
+            if(objecto.TryGetComponent(out VidaEnemigo vidaEnemigo))
+            {
+                vidaEnemigo.TomarDaño(dañoAtaque);            
+            }
+        }
+    }
+
+   private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(controladorAtaque.position, radioAtaque);
+    }
+}
